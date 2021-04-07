cc77cc7c1a42adc8a04e9f4e1e26face2dca67d9
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-04-07 19:20:49 +0200

    [master] UI implementation of User detail view

--- src/views/UserDetailView.js
+++ src/views/UserDetailView.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
-import {StyleSheet, Text, View, ActivityIndicator, SafeAreaView, Button, Image, TouchableOpacity} from "react-native";
-import {Header} from "react-native-elements";
-import {Entypo, MaterialIcons} from "@expo/vector-icons";
+import {
+    StyleSheet, Text, View, SafeAreaView,
+    Image, TouchableOpacity, FlatList
+} from "react-native";
+import {Entypo} from "@expo/vector-icons";
 
 export default class UserDetailView extends Component {
     constructor(props) {
@@ -32,16 +34,46 @@ export default class UserDetailView extends Component {
                         source={require('../resources/gamer.png')}
                         style={styles.userImage}
                     />
+                    <View style={styles.nameBox}>
+                        <Text style={styles.nameText}>{item.name} {item.surname}</Text>
+                        <Text style={styles.usernameText}>{item.username}</Text>
+                    </View>
                 </View>
+                <FlatList style={styles.flatl}
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                  data={item.PaymentMethods}
+                  keyExtractor={(x, i) => i}
+                  renderItem={({item}) => {
+                      return (
+                          <Item item={item}/>
+                      )
+                  }}
+                />
             </SafeAreaView>
         )
     }
 }
 
-const styles= StyleSheet.create({
-    firstBlock: {
+function Item({ item }) {
 
-    },
+    let defaultString = "";
+    if(item.default){
+        defaultString = "default payment"
+    }
+
+    return (
+        <View style={styles.listItem}>
+            <View style={{alignItems:"center",flex:1, justifyContent:"center"}}>
+                <Text style={styles.cardTypeText}>{item.type}</Text>
+                <Text style={styles.defaultCardText}>**** **** **** {item.ending_with}</Text>
+                <Text style={styles.defaultCardText}>{defaultString}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles= StyleSheet.create({
     header: {
         height: 60,
         borderBottomWidth: 0.5,
@@ -57,12 +89,56 @@ const styles= StyleSheet.create({
     },
     headerText: {
         fontSize: 25,
-
+    },
+    firstBlock: {
+        flexDirection: 'row',
+        height: 120,
+        alignSelf: 'stretch',
     },
     userImage: {
-        margin: 20,
+        marginLeft: 20,
         width:80,
         height:80,
-        borderRadius:45
-    }
+        borderRadius:45,
+        justifyContent: 'center',
+        alignSelf: 'center'
+    },
+    nameBox: {
+        alignSelf: 'center',
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    nameText: {
+        fontSize: 25,
+    },
+    usernameText: {
+        fontSize: 13,
+    },
+    cardTypeText: {
+        margin: 5,
+        textAlign: 'center',
+        fontSize: 18,
+        alignSelf: 'center',
+        color:'#ffffff'
+    },
+    defaultCardText: {
+        margin: 5,
+        textAlign: 'center',
+        fontSize: 9,
+        alignSelf: 'center',
+        color:'#ffffff'
+    },
+    flatl: {
+      marginTop: 60,
+    },
+    listItem:{
+        margin: 10,
+        padding:10,
+        height: 90,
+        width: 160,
+        borderRadius:15,
+        backgroundColor:"#4a566b",
+        alignSelf:"center",
+        flexDirection:"row",
+    },
 })
